fbdbfd037670018f81630dc19d858836632b100a
yasuhiko-hato-sbtmp <yasuhiko.hato.sbtmp@gmail.com>
2017-04-02 19:04:49 +0900

    Implemented the moving logic.

--- app/src/main/java/yasuhiko/hato/movingcharacter/LayerService.java
+++ app/src/main/java/yasuhiko/hato/movingcharacter/LayerService.java
@@ -2,7 +2,6 @@ package yasuhiko.hato.movingcharacter;
 
 
 import android.animation.Animator;
-import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.app.Service;
@@ -13,9 +12,7 @@ import android.graphics.Point;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
-import android.renderscript.Sampler;
 import android.util.Log;
-import android.util.StringBuilderPrinter;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -34,7 +31,7 @@ public class LayerService extends Service {
     private final String LOG_TAG = "LayerService";
     private View mView;
     private ViewTreeObserver.OnGlobalLayoutListener mGlobalLayoutListener;
-    Point mDisplaySize;
+    private Point mDisplaySize;
     private WindowManager mWindowManager;
     private WindowManager.LayoutParams mParams;
     private boolean mThreadFlag = true;
@@ -44,8 +41,7 @@ public class LayerService extends Service {
     private float TRAVELING_TIME_MILLI_SEC = 3000;
     private float STOP_TIME_MILLI_SEC = 5000;
     private ImageView mCharacterImageView;
-    private int mImageViewWidth;
-    private int mImageViewHeight;
+    private Point mImageViewSize;
     final Handler mHandlerForMove = new Handler();
     private ValueAnimator mValueAnimator;
     private Runnable mMovingViewRunnable;
@@ -67,6 +63,7 @@ public class LayerService extends Service {
 
         mWindowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         mDisplaySize = getDisplaySize();
+        Log.d(LOG_TAG, "DisplaySize: " + String.valueOf(mDisplaySize.x) + " x " + String.valueOf(mDisplaySize.y));
 
         mParams = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
@@ -88,9 +85,8 @@ public class LayerService extends Service {
         mGlobalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener(){
             @Override
             public void onGlobalLayout() {
-                mImageViewWidth = mView.getWidth();
-                mImageViewHeight = mView.getHeight();
-                Log.d(LOG_TAG, "Robot imageView: " + String.valueOf(mImageViewWidth) + " x " + String.valueOf(mImageViewHeight));
+                mImageViewSize = new Point(mView.getWidth(), mView.getHeight());
+                Log.d(LOG_TAG, "ImageViewSize: " + String.valueOf(mImageViewSize.x) + " x " + String.valueOf(mImageViewSize.y));
                 removeOnGlobalLayoutListener(mView.getViewTreeObserver(), mGlobalLayoutListener);
             }
         };
@@ -107,6 +103,9 @@ public class LayerService extends Service {
                 int action = motionEvent.getAction();
 
                 if(action == MotionEvent.ACTION_MOVE) {
+                    if(mValueAnimator != null && mValueAnimator.isRunning()){
+                        mValueAnimator.end();
+                    }
                     changeImageViewImage(R.drawable.robot_b_u);
 
                     int centerX = x - (mDisplaySize.x / 2);
@@ -116,10 +115,8 @@ public class LayerService extends Service {
                     mParams.y = centerY;
 
                     mWindowManager.updateViewLayout(mView, mParams);
-                    mIsDragged = true;
                 }
                 else if(action == MotionEvent.ACTION_UP) {
-                    mIsDragged = false;
                     changeImageViewImage(R.drawable.robot_b_l);
                 }
                 return false;
@@ -134,7 +131,8 @@ public class LayerService extends Service {
             public void run() {
 
                 if(mThreadFlag) {
-                    mValueAnimator = setAnimation(new Point(mParams.x, mParams.y), 45f, mDisplaySize.x / 4, (long) TRAVELING_TIME_MILLI_SEC);
+                    //mValueAnimator = setPropertyValuesHolderToValueAnimator(new Point(mParams.x, mParams.y), 45f, mDisplaySize.x / 4, (long) TRAVELING_TIME_MILLI_SEC);
+                    mValueAnimator = setAnimation(new Point(mParams.x, mParams.y), mDisplaySize, mImageViewSize);
                     mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                         @Override
                         public void onAnimationUpdate(ValueAnimator valueAnimator) {
@@ -150,7 +148,7 @@ public class LayerService extends Service {
                         @Override
                         public void onAnimationEnd(Animator animator) {
                             mValueAnimator.removeAllUpdateListeners();
-                            Log.d(LOG_TAG, "onAnimationEnd()");
+                            //Log.d(LOG_TAG, "onAnimationEnd()");
                         }
 
                         @Override
@@ -227,7 +225,44 @@ public class LayerService extends Service {
     }
 
 
-    private ValueAnimator setAnimation(Point source, float degree, float distance, long duration ) {
+    private ValueAnimator setAnimation(Point source, Point displaySize, Point viewSize){
+        int currentQuadrant = whichQuadrantInCenterCoordinateSys(source);
+
+        float toX = (float)(Math.random() * (displaySize.x / 2 - viewSize.x / 2));
+        float toY = (float)(Math.random() * (displaySize.y / 2 - viewSize.y / 2));
+        int nextQuadrant = (int)(Math.random() * 4) + 1;
+        if(nextQuadrant == 1){
+
+        }
+        else if(nextQuadrant == 2){
+            toY = -toY;
+        }
+        else if(nextQuadrant == 3){
+            toX = -toX;
+            toY = -toY;
+        }
+        else{
+            toX = -toX;
+        }
+
+        if(toX > source.x){
+            changeImageViewImage(R.drawable.robot_b_r);
+        }
+        else{
+            changeImageViewImage(R.drawable.robot_b_l);
+        }
+
+        PropertyValuesHolder holderX = PropertyValuesHolder.ofFloat("translationX", (float)source.x, toX);
+        PropertyValuesHolder holderY = PropertyValuesHolder.ofFloat("translationY", (float)source.y, toY);
+        ValueAnimator valueAnimator = ValueAnimator.ofPropertyValuesHolder(holderX, holderY);
+        valueAnimator.setDuration((long)TRAVELING_TIME_MILLI_SEC);
+        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        Log.d(LOG_TAG, "Move: " + String.valueOf(currentQuadrant) + "(" + String.valueOf(source.x) + ", " + String.valueOf(source.y) + ") -> " + String.valueOf(nextQuadrant) + "(" + String.valueOf(toX) + ", " + String.valueOf(toY) + ")");
+        return valueAnimator;
+    }
+
+    private ValueAnimator setPropertyValuesHolderToValueAnimator(Point source, float degree, float distance, long duration ) {
 
         // 距離と角度から到達点となるX座標、Y座標を求めます
         float toX = (float) (distance * Math.cos(Math.toRadians(degree)));
@@ -244,17 +279,38 @@ public class LayerService extends Service {
 //        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
 //                target, holderX, holderY, holderRotaion );
         ValueAnimator valueAnimator = ValueAnimator.ofPropertyValuesHolder(holderX, holderY);
-        // 2秒かけて実行させます
+        // duration秒かけて実行させます
         valueAnimator.setDuration(duration);
         valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
 
         return valueAnimator;
     }
 
+
+    /**
+     *
+     * @param p
+     * @return Quadrant whose coordinate system's original point is the center of the display.
+     */
+    private int whichQuadrantInCenterCoordinateSys(Point p){
+        if(p.x >= 0 && p.y >= 0){
+            return 1;
+        }
+        else if(p.x >= 0 && p.y < 0){
+            return 2;
+        }
+        else if(p.x < 0 && p.y < 0){
+            return 3;
+        }
+        else{
+            return 4;
+        }
+    }
+
     private void movingViewUpdateListenerLogic(ValueAnimator valueAnimator){
         float x = (float)valueAnimator.getAnimatedValue("translationX");
         float y = (float)valueAnimator.getAnimatedValue("translationY");
-        Log.d(LOG_TAG, "current coordinate: (" + String.valueOf(x) + ", " + String.valueOf(y) + ")");
+        //Log.d(LOG_TAG, "current coordinate: (" + String.valueOf(x) + ", " + String.valueOf(y) + ")");
         mParams.x = (int)x;
         mParams.y = (int)y;
         mWindowManager.updateViewLayout(mView, mParams);
@@ -286,10 +342,10 @@ public class LayerService extends Service {
                         continue;
                     }
 
-                    if(mParams.x - mImageViewWidth/2 < -mDisplaySize.x/2 || mParams.x + mImageViewWidth/2 > mDisplaySize.x/2) {
+                    if(mParams.x - mImageViewSize.x/2 < -mDisplaySize.x/2 || mParams.x + mImageViewSize.x/2 > mDisplaySize.x/2) {
                         mMoveX = -mMoveX;
                     }
-                    if(mParams.y - mImageViewHeight/2 < -mDisplaySize.y/2 || mParams.y + mImageViewHeight/2 > mDisplaySize.y/2){
+                    if(mParams.y - mImageViewSize.y/2 < -mDisplaySize.y/2 || mParams.y + mImageViewSize.y/2 > mDisplaySize.y/2){
                         mMoveY = -mMoveY;
                     }
 
